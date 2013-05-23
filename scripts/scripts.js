5cb610e423596a3f16c37abb34d52af33bb73d48
Travis Kochel <travis@scribbletone.com>
2013-05-23 07:50:12 -0700

    quick sync for class

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,13 @@
 
 $(document).ready(function(){
 
-
-
-
-
+	$('.button').click(function() {
+		var my_image = $(this).closest('.column').html();
+		
+		$('.column').html(my_image);
+		//alert(my_image);
+		
+	});
 
 });
 
